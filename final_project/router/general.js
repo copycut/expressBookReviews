82b490b34396949719618a83f769c1fffd7e2d97
Didier Chartrain <copycut.design@gmail.com>
2025-12-25 12:21:36 +0100

    task 4: get book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,10 +29,11 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
+  const cleanAuthor = decodeURIComponent(author);
   const results = [];
 
   for (const isbn in books) {
-    if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+    if (books[isbn].author.toLowerCase() === cleanAuthor.toLowerCase()) {
       results.push(books[isbn]);
     }
   }
@@ -46,6 +47,25 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  const cleanTitle = decodeURIComponent(title);
+  const results = [];
+
+  for (const isbn in books) {
+    if (books[isbn].title.toLowerCase() === cleanTitle.toLowerCase()) {
+      results.push(books[isbn]);
+    }
+  }
+
+  if (results.length > 0) {
+    return res.status(200).json(results);
+  } else {
+    return res.status(404).json({ message: 'No books found with that title' });
+  }
+});
+
+//  Get book review
+public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
   return res.status(300).json({ message: 'Yet to be implemented' });
 });
