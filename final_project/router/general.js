dc11e4af6d0472e63e7525ce55cb6a552ba99dc9
Didier Chartrain <copycut.design@gmail.com>
2025-12-25 12:10:11 +0100

    task 3 : get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,20 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const author = req.params.author;
+  const results = [];
+
+  for (const isbn in books) {
+    if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+      results.push(books[isbn]);
+    }
+  }
+
+  if (results.length > 0) {
+    return res.status(200).json(results);
+  } else {
+    return res.status(404).json({ message: 'No books found by that author' });
+  }
 });
 
 // Get all books based on title
