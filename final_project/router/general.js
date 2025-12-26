b467ce82de6c4550d72221f62d65fe5a02ebb08f
Didier Chartrain <copycut.design@gmail.com>
2025-12-26 11:30:54 +0100

    task 10: async books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let books = require('./booksdb.js');
+let getBooks = require('./booksdb.js').getBooks;
 let isValid = require('./auth_users.js').isValid;
 let users = require('./auth_users.js').users;
 const public_users = express.Router();
@@ -10,13 +10,15 @@ public_users.post('/register', (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
+public_users.get('/', async function (req, res) {
+  const books = await getBooks();
   res.status(200).json(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
+  const books = await getBooks();
   const book = books[isbn];
 
   if (book) {
@@ -27,13 +29,14 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
   const cleanAuthor = decodeURIComponent(author);
+  const books = await getBooks();
   const results = [];
 
   for (const isbn in books) {
-    if (books[isbn].author.toLowerCase() === cleanAuthor.toLowerCase()) {
+    if (books[isbn]?.author?.toLowerCase() === cleanAuthor.toLowerCase()) {
       results.push(books[isbn]);
     }
   }
@@ -46,9 +49,10 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
   const cleanTitle = decodeURIComponent(title);
+  const books = await getBooks();
   const results = [];
 
   for (const isbn in books) {
@@ -65,8 +69,9 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn', function (req, res) {
+public_users.get('/review/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
+  const books = await getBooks();
   const book = books[isbn];
 
   if (book?.reviews) {
